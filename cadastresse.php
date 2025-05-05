f0f2acb989316bb3fafc22b9dcd8f3a1043990c7
Aidan-G-de-Lima <aidan.lima@aluno.senai.br>
2025-05-05 14:30:25 -0300

    curso senai

---
+++ cadastresse.php
@@ -0,0 +1,13 @@
+<?php
+
+$nome = $_POST["nome"];
+
+$email = $_POST["email"];
+
+$telefone = $_POST["telefone"];
+
+$senha = $_POST["senha"];
+
+echo "$senha .<br> $telefone .<br> $email  .<br> $nome .<br>";
+
+?>
---
+++ contacto.php
@@ -0,0 +1,12 @@
+<?php
+
+
+$email = $_POST["email"];
+
+$telefone = $_POST["telefone"];
+
+$Comentario = $_POST["Comentario"];
+
+echo "<br> $telefone .<br> $email.  <br>$Comentario. ";
+
+?>
